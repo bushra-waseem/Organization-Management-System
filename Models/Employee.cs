050fb11e0683afe0ab75519772a8271fe9724f81
Bushra Waseem <bushrawaseem951@gmail.com>
2026-03-31 02:20:33 +0500

    initial project upload

---
+++ Models/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dapper_Company.Models
+{ 
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Gender { get; set; } = string.Empty;
+        public int OrganizationId { get; set; }
+
+        public string? OrganizationName { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public string? DepartmentName { get; set; }
+
+        public int PositionId { get; set; }
+
+        public string? PositionName { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
+
+
+
+//namespace Dapper_Company.Models
+//{
+//    public class Employee
+//    {
+//        public int Id { get; set; }
+//        public string Name { get; set; }
+//        public int OrganizationId { get; set; }
+//        public string OrganizationName { get; set; }
+//        public Organization Organization { get; set; }
+//        public int DepartmentId { get; set; }
+//        public string DepartmentName { get; set; }
+//        public Department Department { get; set; }
+//        public int PositionId { get; set; }
+//        public string PositionName { get; set; }
+//        public Position Position { get; set; }
+//        public string Gender { get; set; }
+//        [Required]
+//        [Range(0, double.MaxValue)]
+//        public decimal Salary { get; set; }
+//    }
+//}
